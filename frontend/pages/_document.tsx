bc081139144a2aa6b0108b8d4a2c36cd7feecd84
thomashacker <EdwardSchmuhl@web.de>
2024-03-15 16:20:27 +0100

    Add google analytics

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="icon" href="/favicon.png" />
+                <GoogleAnalytics gaId="G-QDWDLFK50G" />
             </Head>
             <body>
                 <Main />
