7ccea87acae472088937f026e7a01b80a1f8b2ed
thomashacker <EdwardSchmuhl@web.de>
2024-03-15 16:53:47 +0100

    Fix google_tag

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
-            <GoogleTagManager gtmId="G-QDWDLFK50G" />
+            <GoogleAnalytics gaId="G-QDWDLFK50G" />
         </>
     )
 }
